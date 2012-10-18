6326049f11b457de6fd94a8a070a7a10bf18501e
Ben Peddell <klightspeed@killerwolves.net>
2012-10-18 18:15:20 +1000

    Logging

--- vSphereHostShutdown/Program.cs
+++ vSphereHostShutdown/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Net;
 using System.Xml;
 using System.IO;
+using System.Diagnostics;
 
 namespace vSphereHostShutdown
 {
@@ -17,11 +18,21 @@ namespace vSphereHostShutdown
                 Console.WriteLine("Usage: {0} <server> <username> <password>", System.Reflection.Assembly.GetExecutingAssembly().GetName());
                 return;
             }
+
+            if (!EventLog.SourceExists("vSphereHostShutdown"))
+            {
+                EventLog.CreateEventSource("vSphereHostShutdown", "application");
+            }
+
+            EventLog.WriteEntry("vSphereHostShutdown", "Initiating host shutdown", EventLogEntryType.Information);
+
             try
             {
                 Vim25Client client = new Vim25Client(args[0], args[1], args[2]);
                 client.LogUserEvent("HostSystem", "ha-host", "Shutdown Test");
                 client.ShutdownHost_Task(true);
+                Console.WriteLine("Shutdown initiated");
+                EventLog.WriteEntry("vSphereHostShutdown", "Host shutdown initiated", EventLogEntryType.Information);
             }
             catch (WebException ex)
             {
@@ -32,14 +43,20 @@ namespace vSphereHostShutdown
                     {
                         string xml = new System.IO.StreamReader(ex.Response.GetResponseStream()).ReadToEnd();
                         Console.Write(xml);
+                        EventLog.WriteEntry("vShpereHostShutdown", "Host shutdown failed\n\nReturned XML:\n" + xml, EventLogEntryType.Error);
                         return;
                     }
+                }
+                else
+                {
+                    EventLog.WriteEntry("vSphereHostShutdown", "HTTP Exception caught attempting host shutdown\n\nException details:\n" + ex.ToString(), EventLogEntryType.Error);
                 }
                 Console.Write(ex.ToString());
             }
             catch (Exception ex)
             {
                 Console.Write(ex.ToString());
+                EventLog.WriteEntry("vSphereHostShutdown", "Exception caught attempting host shutdown\n\nException details:\n" + ex.ToString(), EventLogEntryType.Error);
             }
         }
     }
